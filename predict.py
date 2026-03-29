13f6d56c43144daefad06681f00a9ca4aa4f34b5
DHARNIDHARAN-SK <karnan284858@gmail.com>
2026-03-29 20:26:34 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 # =========================
 # CONFIG
 # =========================
-MODEL_PATH = "model_fixed.keras"
+MODEL_PATH = "model_fixed.h5"
 SCALER_PATH = "scaler.pkl"
 
 SEQ_LEN = 10
